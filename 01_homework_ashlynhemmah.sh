583a955417017f584f9c22aff27a7c664eb98fdd
ashlynhemmah <ashlyn.hemmah@gmail.com>
2026-02-05 11:51:47 -0700

    Homework 1 and 2

---
+++ 01_homework_ashlynhemmah.sh
@@ -0,0 +1,51 @@
+
+**HOMEWORK W1**
+
+
+
+cd Desktop/Genomics/
+
+curl -L -O https://raw.githubusercontent.com/elinck/genomics\_eco\_con/main/data/week\_1.tar.gz -o week\_1.tar.gz # download a resource at a url
+
+tar -xzf week\_1.tar.gz # this command unzips a "tarball"
+
+cd week\_1 # enter the data directory
+
+
+
+ls
+
+
+
+mkdir fastq/;
+
+mkdir fasta/;
+
+mkdir metadata/;
+
+
+
+ls
+
+
+
+mv \*.fastq.gz fastq/ #8 files 
+
+ls
+
+
+
+mv \*.fasta fasta/ #3 files
+
+ls
+
+
+
+mv \*.csv metadata/ #3 files 
+
+ls
+
+
+
+mv week\_1/ ~/Desktop/Genomics/Homework/ #week 1 initially went into the test folder so I moved it out 
+
